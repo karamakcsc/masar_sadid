7f5f660b423d4303015e57ae90d838a9b7eeaafa
karamakcsc <karamakcsc@hotmail.com>
2025-12-10 23:24:15 +0300

    update

--- masar_sadid/api.py
+++ masar_sadid/api.py
@@ -1,35 +1,63 @@
 import frappe
 from googletrans import Translator
 
+# @frappe.whitelist()
+# def translate_to_arabic(text):
+#     """Translate English text to Arabic using free Google Translate"""
+#     if not text:
+#         return ""
+
+#     try:
+#         translator = Translator()
+#         translated = translator.translate(text, src='en', dest='ar')
+#         return translated.text
+
+#     except Exception as e:
+#         frappe.log_error(frappe.get_traceback(), "Translation Error")
+#         return "Translation failed"
+
+
+
+
+# @frappe.whitelist()
+# def translate_to_english(text):
+#     """Translate Arabic text to English using free Google Translate"""
+#     if not text:
+#         return ""
+
+#     try:
+#         translator = Translator()
+#         translated = translator.translate(text, src='ar', dest='en')
+#         return translated.text
+
+#     except Exception as e:
+#         frappe.log_error(frappe.get_traceback(), "Translation Error")
+#         return "Translation failed"
+
 @frappe.whitelist()
 def translate_to_arabic(text):
-    """Translate English text to Arabic using free Google Translate"""
+    """Translate English text to Arabic"""
     if not text:
         return ""
 
     try:
-        translator = Translator()
-        translated = translator.translate(text, src='en', dest='ar')
-        return translated.text
+        translated = Translator(source='en', target='ar').translate(text)
+        return translated
 
     except Exception as e:
-        frappe.log_error(frappe.get_traceback(), "Translation Error")
+        frappe.log_error(frappe.get_traceback(), "Translation Error (EN to AR)")
         return "Translation failed"
-
-
-
 
 @frappe.whitelist()
 def translate_to_english(text):
-    """Translate Arabic text to English using free Google Translate"""
+    """Translate Arabic text to English"""
     if not text:
         return ""
 
     try:
-        translator = Translator()
-        translated = translator.translate(text, src='ar', dest='en')
-        return translated.text
+        translated = Translator(source='ar', target='en').translate(text)
+        return translated
 
     except Exception as e:
-        frappe.log_error(frappe.get_traceback(), "Translation Error")
+        frappe.log_error(frappe.get_traceback(), "Translation Error (AR to EN)")
         return "Translation failed"
--- masar_sadid/custom/journal_entry/journal_entry.js
+++ masar_sadid/custom/journal_entry/journal_entry.js
@@ -1,39 +1,59 @@
 frappe.ui.form.on("Journal Entry", {
     user_remark: function(frm) {
+        if (frm.translating) return;
+        
+        // إلغاء أي ترجمة سابقة معلقة
+        if (frm.translation_timeout) {
+            clearTimeout(frm.translation_timeout);
+        }
+        
         if (frm.doc.user_remark) {
-            frappe.call({
-                method: "masar_sadid.api.translate_to_arabic",
-                args: {
-                    text: frm.doc.user_remark
-                },
-                callback: function(r) {
-                    if (r.message) {
-                        frm.set_value("custom_user_remark_ar", r.message);
+            // تأخير الترجمة لمدة 500ms
+            frm.translation_timeout = setTimeout(function() {
+                frm.translating = true;
+                
+                frappe.call({
+                    method: "masar_sadid.api.translate_to_arabic",
+                    args: {
+                        text: frm.doc.user_remark
+                    },
+                    callback: function(r) {
+                        if (r.message) {
+                            frm.set_value("custom_user_remark_ar", r.message);
+                        }
+                        frm.translating = false;
                     }
-                }
-            });
+                });
+            }, 500);
         } else {
             frm.set_value("custom_user_remark_ar", "");
         }
-    }
-});
+    },
 
-
-
-frappe.ui.form.on("Journal Entry", {
     custom_user_remark_ar: function(frm) {
+        if (frm.translating) return;
+        
+        if (frm.translation_timeout) {
+            clearTimeout(frm.translation_timeout);
+        }
+        
         if (frm.doc.custom_user_remark_ar) {
-            frappe.call({
-                method: "masar_sadid.api.translate_to_english",
-                args: {
-                    text: frm.doc.custom_user_remark_ar
-                },
-                callback: function(r) {
-                    if (r.message) {
-                        frm.set_value("user_remark", r.message);
+            frm.translation_timeout = setTimeout(function() {
+                frm.translating = true;
+                
+                frappe.call({
+                    method: "masar_sadid.api.translate_to_english",
+                    args: {
+                        text: frm.doc.custom_user_remark_ar
+                    },
+                    callback: function(r) {
+                        if (r.message) {
+                            frm.set_value("user_remark", r.message);
+                        }
+                        frm.translating = false;
                     }
-                }
-            });
+                });
+            }, 500);
         } else {
             frm.set_value("user_remark", "");
         }
